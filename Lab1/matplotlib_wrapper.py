557841ec98fce279906645555ca14de072636088
zexi26 <yuzexi26@gmail.com>
2019-06-28 02:52:00 -0400

    Update matplotlib_wrapper.py

--- Lab1/matplotlib_wrapper.py
+++ Lab1/matplotlib_wrapper.py
@@ -1,7 +1,21 @@
 import matplotlib.pyplot as plt
 
-def show():
-    plt.show()
 
 def hist(x):
-    plt.hist(x)
+    pos = list(range(len(x[0])))
+    width = 0.15
+    fig, ax = plt.subplots(figsize=(20, 10))
+    
+    for i in range(len(x)):
+        plt.bar([p + i * width for p in pos], freq[i], width, alpha=0.5)
+        
+    ax.set_xticks([p + len(x) * width / 3 for p in pos])
+    ax.set_xticklabels(
+        ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+         'w', 'x', 'y', 'z'])
+    
+    plt.ylabel('Frequency')
+    plt.title('Frequency of letters')
+    plt.grid()
+    plt.show()
+    
