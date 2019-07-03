ce432e791b30a6be948d77116648308c4df00430
Saffat Hasan <saffathasan@gmail.com>
2019-07-03 00:09:40 -0400

    Updated ex2 to take command line arguments

--- Lab1/ex2.py
+++ Lab1/ex2.py
@@ -12,13 +12,23 @@ Description:
 from frequencyAnalysis import *
 import urllib_wrapper
 import matplotlib_wrapper as plt
+import sys
 
-books = [
-    "https://www.gutenberg.org/files/59780/59780-0.txt",
-    "https://www.gutenberg.org/files/59802/59802-0.txt",
+books_ids = [
+    59780,
+    59802
 ]
 
+BOOK_URL = "https://www.gutenberg.org/files/{0}/{0}-0.txt"
+
 if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        print("Using book IDs: {}".format(sys.argv[1:]))
+        books = [BOOK_URL.format(item) for item in sys.argv[1:]]
+    else:
+        print("No command line arguments specified.")
+        print("Using default book set: {}".format(books_ids))
+        books = [BOOK_URL.format(item) for item in books_ids]
     frequencies = []
     analyzer = FrequencyAnalysisByLetter()
     for i in range(len(books)):
@@ -27,11 +37,12 @@ if __name__ == "__main__":
         frequencyDictionary = analyzer.getNormalizedFrequencyDict(file_name)
         frequencies.append(frequencyDictionary)
 
-    frequency_values_1 = list(frequencies[0].values())
-    frequency_values_2 = list(frequencies[1].values())
+    if len(frequencies) > 1:
+        frequency_values_1 = list(frequencies[0].values())
+        frequency_values_2 = list(frequencies[1].values())
 
-    rmse = RMSE(frequency_values_1, frequency_values_2)
-    print("The RMSE between these two books is {:.10f}".format(rmse))
+        rmse = RMSE(frequency_values_1, frequency_values_2)
+        print("The RMSE between the first two books is {:.10f}".format(rmse))
 
     values = [list(f.values()) for f in frequencies]
 
