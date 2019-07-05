538910e21af58f906df7efdff3bc7064fa640f59
Saffat Hasan <saffathasan@gmail.com>
2019-07-04 22:20:08 -0400

    Capture code given by Professor Johnson

---
+++ Lab2/prime.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+   if (n < 2):
+     return False
+   d = 2
+   while d*d <= n:
+     if (n % d) == 0:
+       return False
+     d = d+1
+   return True
+
+def sum_primes(a, b):
+  accum = 0
+  for x in range(a, b+1):
+    if is_prime(x):
+      accum += x
+  return accum
