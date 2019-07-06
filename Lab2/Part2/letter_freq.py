664e3961fbc2d7cf073982546e2ea114e2728976
Evan Lavender <evanlavender@pop-os.localdomain>
2019-07-06 10:38:42 -0400

    Added Part2 directory Implemented naive letter frequency job. Should it calc the frequency, or is counting good enough? Added some input text

---
+++ Lab2/Part2/letter_freq.py
@@ -0,0 +1,19 @@
+import re
+
+from mrjob.job import MRJob
+
+WORD_RE = re.compile("[a-zA-Z]")
+
+
+class MRLetterFrequency(MRJob):
+
+    def mapper(self, _, line):
+        for letter in WORD_RE.findall(line):
+            yield letter.lower(), 1
+
+    def reducer(self, letter, counts):
+        yield letter, sum(counts)
+
+
+if __name__ == '__main__':
+    MRLetterFrequency.run()
