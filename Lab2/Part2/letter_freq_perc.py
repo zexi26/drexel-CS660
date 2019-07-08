38e73ca73031cc3579a40c828a54e04bd3d48a8d
Evan Lavender <evanlavender@pop-os.localdomain>
2019-07-08 18:50:59 -0400

    Added letter frequency job that calculates percentage

---
+++ Lab2/Part2/letter_freq_perc.py
@@ -0,0 +1,40 @@
+import re
+
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+
+LETTER_RE = re.compile("[a-zA-Z]")
+
+
+class MRLetterFrequency(MRJob):
+
+    def mapper(self, _, line):
+        for letter in LETTER_RE.findall(line):
+            yield letter.lower(), 1
+
+    def combiner(self, letter, counts):
+        yield letter, sum(counts)
+
+    def reducer(self, letter, counts):
+        yield None, (sum(counts), letter)
+
+    def reducer_freq(self, _, pairs):
+        pairs = sorted(list(pairs))
+
+        total = sum([pair[0] for pair in pairs])
+
+        for pair in reversed(pairs):
+            pair[0] /= total
+            yield pair
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper,
+                   combiner=self.combiner,
+                   reducer=self.reducer),
+            MRStep(reducer=self.reducer_freq)
+        ]
+
+
+if __name__ == '__main__':
+    MRLetterFrequency.run()
