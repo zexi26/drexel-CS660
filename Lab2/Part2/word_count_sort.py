f53f9cbfeb00efa0880c1503acb52beceae38bbe
zexi26 <yuzexi26@gmail.com>
2019-07-08 13:04:48 -0400

    Create word_count_sort.py

---
+++ Lab2/Part2/word_count_sort.py
@@ -0,0 +1,35 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+import re
+
+WORD_RE = re.compile(r"[\w']+")
+
+
+class MRWordCountSort(MRJob):
+
+    def mapper_get_words(self, _, line):
+        for word in WORD_RE.findall(line):
+            yield (word.lower(), 1)
+
+    def combiner_count_words(self, word, counts):
+        # sum the words we've seen so far
+        yield (word, sum(counts))
+
+    def reducer_count_words(self, word, counts):
+        yield None, (sum(counts), word)
+
+    def reducer_sort_word_count(self, _, word_count_pairs):
+        for v in sorted(list(word_count_pairs)):
+            yield v
+
+    def steps(self):
+        return [
+            MRStep(mapper=self.mapper_get_words,
+                   combiner=self.combiner_count_words,
+                   reducer=self.reducer_count_words),
+            MRStep(reducer=self.reducer_sort_word_count)
+        ]
+
+
+if __name__ == '__main__':
+    MRWordCountSort.run()
