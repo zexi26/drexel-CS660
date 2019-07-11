8aa95241c67cc2b7be725525e486795e33d47152
Saffat Hasan <saffathasan@gmail.com>
2019-07-10 20:42:43 -0400

    Updated to use python scripts instead of makefile targets

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,11 +1,21 @@
 import requests
 
 
-def downloadFile(fileName):
+def downloadFile(file_name):
     response = requests.get("http://www.gutenberg.org/files/2600/2600-0.txt")
-    with open(fileName, 'wb') as f:
+    with open(file_name, 'wb') as f:
         f.write(response.content)
+
+
+def duplicateFile(source, num_copies):
+    destination_file_name = "{}_{}".format(num_copies, source)
+    with open(destination_file_name, 'wb') as destination:
+        for i in range(1000):
+            with open(source, 'rb') as source_file:
+                for line in source_file:
+                    destination.write(line)
 
 
 if __name__ == "__main__":
     downloadFile("sampleBook.txt")
+    duplicateFile("sampleBook.txt", 1000)
