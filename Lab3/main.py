09f643845ddaf668a4a5b16c62b3175684841560
Evan Lavender <evanlavender@pop-os.localdomain>
2019-07-14 10:59:53 -0400

    Revert "Made the copy function a little more efficient"
    This reverts commit 11ec0d5f6f534a9b85c17bd21aaa88dce066f022.

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,27 +1,25 @@
+import requests
 import sys
 
-import requests
+num_copies = sys.argv[1]
 
 
-def download_file(file_name):
+def downloadFile(file_name):
     response = requests.get("http://www.gutenberg.org/files/2600/2600-0.txt")
     with open(file_name, 'wb') as f:
         f.write(response.content)
 
 
-def duplicate_file(source, num_copies):
+def duplicateFile(source, num_copies):
     destination_file_name = "{}_{}".format(num_copies, source)
-
-    with open(source, 'r') as in_file, open(destination_file_name, 'w') as out_file:
-        big_string = ""
-        text = in_file.read()
+    with open(destination_file_name, 'wb') as destination:
         for i in range(num_copies):
-            big_string += text
-
-        out_file.write(big_string)
+            with open(source, 'rb') as source_file:
+                for line in source_file:
+                    destination.write(line)
 
 
 if __name__ == "__main__":
-    num_copies = int(sys.argv[1])
-    download_file("sampleBook.txt")
-    duplicate_file("sampleBook.txt", num_copies)
+    num_copies = sys.argv[1]
+    downloadFile("sampleBook.txt")
+    duplicateFile("sampleBook.txt", num_copies)
