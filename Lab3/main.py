11ec0d5f6f534a9b85c17bd21aaa88dce066f022
Evan Lavender <evanlavender@pop-os.localdomain>
2019-07-14 10:58:15 -0400

    Made the copy function a little more efficient

--- Lab3/main.py
+++ Lab3/main.py
@@ -1,25 +1,27 @@
-import requests
 import sys
 
-num_copies = sys.argv[1]
+import requests
 
 
-def downloadFile(file_name):
+def download_file(file_name):
     response = requests.get("http://www.gutenberg.org/files/2600/2600-0.txt")
     with open(file_name, 'wb') as f:
         f.write(response.content)
 
 
-def duplicateFile(source, num_copies):
+def duplicate_file(source, num_copies):
     destination_file_name = "{}_{}".format(num_copies, source)
-    with open(destination_file_name, 'wb') as destination:
+
+    with open(source, 'r') as in_file, open(destination_file_name, 'w') as out_file:
+        big_string = ""
+        text = in_file.read()
         for i in range(num_copies):
-            with open(source, 'rb') as source_file:
-                for line in source_file:
-                    destination.write(line)
+            big_string += text
+
+        out_file.write(big_string)
 
 
 if __name__ == "__main__":
-    num_copies = sys.argv[1]
-    downloadFile("sampleBook.txt")
-    duplicateFile("sampleBook.txt", num_copies)
+    num_copies = int(sys.argv[1])
+    download_file("sampleBook.txt")
+    duplicate_file("sampleBook.txt", num_copies)
