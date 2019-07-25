3b0117a216713710e6b7a7bb7be553a919e4eecd
Evan Lavender <evanlavender@pop-os.localdomain>
2019-07-24 20:02:37 -0400

    Implemented mapper_raw version, added some documentation

--- Lab4/inverted_index.py
+++ Lab4/inverted_index.py
@@ -7,23 +7,76 @@ from mrjob.job import MRJob
 WORD_RE = re.compile("[\w']+")
 
 
-class InvertedIndexBaseline(MRJob):
+class InvertedIndexRaw(MRJob):
+    SORT_VALUES = True
 
-    def mapper_init(self):
-        self.document_id = int(os.path.basename(os.environ["map_input_file"]).split(".")[0])
+    def mapper_raw(self, input_path, input_uri):
+        """Mapper function
 
-    def mapper(self, _, json):
+        Args:
+            input_path (str): a local path that the input file has been copied to
+            input_uri (str): the URI of the input file on HDFS, S3, etc
+
+        Yields:
+           str, (str, int): a term paired with a tuple containing the ``document_id`` and term frequency
+
+        """
         self.increment_counter("mapper", "calls", 1)
 
-        document, document_id, h = json, self.document_id, defaultdict(int)
+        document_id, h = os.path.basename(input_path).split(".")[0], defaultdict(int)
 
-        for term in WORD_RE.findall(document):
+        with open(input_path, "r") as file:
+            for term in WORD_RE.findall(file.read()):
+                h[term] += 1
+
+            for term, frequency in h.items():
+                yield term, (document_id, frequency)
+
+    def reducer(self, key, values):
+        self.increment_counter("reducer", "calls", 1)
+
+        yield key, [value for value in values]
+
+
+class InvertedIndexBaseline(MRJob):
+    """This job will run the baseline inverted index algorithm from section 4.3"""
+
+    def mapper_init(self):
+        """Retrieves ``document_id`` from input file name"""
+        self.document_id = int(os.path.basename(os.environ["map_input_file"]).split(".")[0])
+
+    def mapper(self, _, line):
+        """Mapper function
+
+        Args:
+            _ (None): ignored
+            line (str): a line of text from the input
+
+        Yields:
+           str, (str, int): a term paired with a tuple containing the ``document_id`` and term frequency
+
+        """
+        self.increment_counter("mapper", "calls", 1)
+
+        h = defaultdict(int)
+
+        for term in WORD_RE.findall(line):
             h[term] += 1
 
         for term, frequency in h.items():
-            yield term, (document_id, frequency)
+            yield term, (self.document_id, frequency)
 
     def reducer(self, term, values):
+        """Reducer function
+
+        Args:
+            term (str): a term
+            values (generator): a list of tuples from the mapper
+
+        Yields:
+            str, OrderedDict: a term paired with an ``OrderedDict`` of postings (doc_id, tf)
+
+        """
         self.increment_counter("reducer", "calls", 1)
 
         postings = defaultdict(int)
@@ -37,23 +90,46 @@ class InvertedIndexBaseline(MRJob):
 
 
 class InvertedIndexRevised(MRJob):
+    """This job will run the revised inverted index algorithm from section 4.4"""
+
     SORT_VALUES = True
 
     def mapper_init(self):
-        self.document_id = int(os.path.basename(os.environ["map_input_file"]).split(".")[0])
+        """Retrieves ``document_id`` from input file name"""
+        self.document_id = os.path.basename(os.environ["map_input_file"]).split(".")[0]
 
-    def mapper(self, _, json):
+    def mapper(self, _, line):
+        """Mapper function
+
+        Args:
+            _ (None): ignored
+            line (str): a line of text from the input
+
+        Yields:
+           str, (str, int): a term paired with a tuple containing the ``document_id`` and term frequency
+
+        """
         self.increment_counter("mapper", "calls", 1)
 
-        document, document_id, h = json, self.document_id, defaultdict(int)
+        h = defaultdict(int)
 
-        for term in WORD_RE.findall(document):
+        for term in WORD_RE.findall(line):
             h[term] += 1
 
         for term, frequency in h.items():
-            yield term, (document_id, frequency)
+            yield term, (self.document_id, frequency)
 
     def reducer(self, term, values):
+        """Reducer function
+
+        Args:
+            term (str): a term
+            values (generator): a list of tuples from the mapper
+
+        Yields:
+            str, defaultdict: a term paired with a ``defaultdict`` of postings (doc_id, tf)
+
+        """
         self.increment_counter("reducer", "calls", 1)
 
         postings = defaultdict(int)
@@ -68,4 +144,5 @@ class InvertedIndexRevised(MRJob):
 
 if __name__ == "__main__":
     # InvertedIndexBaseline.run()
-    InvertedIndexRevised.run()
+    # InvertedIndexRevised.run()
+    InvertedIndexRaw.run()
