a6337366498fcbacfe7a2fa1638290c8a5a6ffd4
Evan Lavender <evanlavender@pop-os.localdomain>
2019-08-16 10:41:41 -0400

    Starting some stuff

---
+++ Lab5/README.md
@@ -0,0 +1,7 @@
+# CS 660 Lab 5
+## Collaborators
+- Evan Lavender
+- Merlin Cherian
+- Saffat Hasan
+- Zexi Yu
+
---
+++ Lab5/main.py
@@ -0,0 +1,56 @@
+"""
+# CS660 Lab 5
+## Collaborators
+- Evan Lavender
+- Merlin Cherian
+- Saffat Hasan
+- Zexi Yu
+
+## Usage
+
+```bash
+spark-submit --packages graphframes:graphframes:0.7.0-spark2.4-s_2.11 main.py
+```
+
+"""
+
+import io
+from contextlib import redirect_stdout
+
+import handout
+from graphframes import GraphFrame
+from pyspark.shell import sqlContext
+
+doc = handout.Handout("output")
+
+""" ## Create some edges and vertices """
+vertices = sqlContext.createDataFrame([
+    (1,),
+    (2,),
+    (3,),
+    (4,)],
+    ["id"])
+
+edges = sqlContext.createDataFrame([
+    (1, 2),
+    (1, 3),
+    (2, 3),
+    (2, 4),
+    (3, 1),
+    (4, 1),
+    (4, 3)],
+    ["src", "dst"])
+
+graph = GraphFrame(vertices, edges)
+
+""" ## Show Vertices """
+with io.StringIO() as buf, redirect_stdout(buf):
+    graph.vertices.show()
+    doc.add_text(buf.getvalue())
+doc.show()
+
+""" ## Show Edges """
+with io.StringIO() as buf, redirect_stdout(buf):
+    graph.edges.show()
+    doc.add_text(buf.getvalue())
+doc.show()
