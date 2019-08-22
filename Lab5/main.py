0b51f9868f32872e3df21ad0909f064e94d1a80c
Saffat Hasan <saffathasan@gmail.com>
2019-08-21 20:28:13 -0400

    Finalized PageRank

--- Lab5/main.py
+++ Lab5/main.py
@@ -24,13 +24,25 @@ from pyspark.shell import sqlContext
 doc = handout.Handout("handout")
 
 """ ## Helper functions """
+
+
+def print(message):
+    """ Overwrite print in this context """
+    redirect_to_handout(message)
+
+
+def redirect_to_handout(message):
+    doc.add_text(message)
+
+
 def display_graph(item):
     # Redirects Standard Out to the document
     with io.StringIO() as buf, redirect_stdout(buf):
         item.show()
-        doc.add_text(buf.getvalue())
+        redirect_to_handout(buf.getvalue())
 
-""" ## Create some edges and vertices """
+
+""" ## Create some edges and vertices to match Fig 2.1 in the paper """
 vertices = sqlContext.createDataFrame([
     (1,),
     (2,),
@@ -41,6 +53,7 @@ vertices = sqlContext.createDataFrame([
 edges = sqlContext.createDataFrame([
     (1, 2),
     (1, 3),
+    (1, 4),
     (2, 3),
     (2, 4),
     (3, 1),
@@ -62,13 +75,51 @@ doc.show()
 display_graph(graph.degrees)
 doc.show()
 
-""" ## Get pagerank using m=0.15 and tolerance=0.01 """
+""" ## Get pagerank using m=0.15 and tolerance=0.01
+"""
 pr = graph.pageRank(resetProbability=0.15, tol=0.01)
 
 """ ### look at the pagerank score for every vertex """
 display_graph(pr.vertices)
 doc.show()
 
-""" ### look at the weight of every edge """
+""" ### look at the weight of every edge
+"""
 display_graph(pr.edges)
 doc.show()
+
+
+""" We can compare the results as follows: """
+# GraphFrames rankings sum to N where N is the number of nodes
+graphframes_pagerank = {
+    1: 1.4645853473254988,
+    2: 0.5777965605200768,
+    3: 1.1469145091124107,
+    4: 0.8107035830420137
+}
+
+# Divide by total to match Google
+total = sum(graphframes_pagerank.values())
+for key in graphframes_pagerank:
+    graphframes_pagerank[key] /= total
+
+# Google rankings sum to 1
+google_pagerank = {
+    1: 0.368,
+    2: 0.142,
+    3: 0.288,
+    4: 0.202
+}
+
+# Pretty Print
+print("+-------+---------------------+")
+print("|Google\t|GraphFrames\t|Delta|")
+print("+-------+---------------------+")
+for key in google_pagerank:
+    google = google_pagerank[key]
+    g_frames = graphframes_pagerank[key]
+    print("|{}\t|{:.3f}\t\t|{:.3f}|".format(google, g_frames, abs(google - g_frames)))
+print("+-------+---------------------+")
+doc.show()
+
+""" As we can see the results of both algorithms are quite similar """
