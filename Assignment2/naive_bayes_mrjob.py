47f7b839d04fb802d0bc334c95c4d9efc4b5ebaf
Evan Lavender <evanlavender@pop-os.localdomain>
2019-08-21 21:02:51 -0400

    Some changes

--- Assignment2/naive_bayes_mrjob.py
+++ Assignment2/naive_bayes_mrjob.py
@@ -6,10 +6,13 @@ from mrjob.protocol import JSONProtocol
 
 WORD_RE = re.compile(r'\w+')
 
+
 class NaiveBayesJob(MRJob):
     INPUT_PROTOCOL = JSONProtocol
+    SORT_VALUES = True
 
     def mapper(self, category, email):
+        self.increment_counter("calls", "mapper", 1)
 
         total_count = 0
         frequencies = {}
@@ -20,12 +23,28 @@ class NaiveBayesJob(MRJob):
             total_count += 1
             frequencies[category][token] += 1
 
-        for category, word_frequencies in frequencies.items():
-            for word, frequency in word_frequencies.items():
-                yield category, (word, frequency)
+        # A's will arrive at the reducer first with SORTED_VALUES = True
+        yield category, ("A", total_count)
 
-    def reducer(self, category, word_frequencies):
-        yield category, list(word_frequencies)
+        for category, token_frequencies in frequencies.items():
+            for token, frequency in token_frequencies.items():
+                yield category, ("B", (token, frequency))
+
+    def reducer(self, category, token_frequencies):
+        self.increment_counter("calls", "reducer", 1)
+
+        total = 0
+        frequencies = defaultdict(int)
+        for key, values in token_frequencies:
+            if key == "A":
+                total += values
+            else:
+                token, frequency = values
+                frequencies[token] += frequency
+
+        yield category, total
+        yield category, frequencies
+        # yield category, list(token_frequencies)
 
 
 if __name__ == "__main__":
